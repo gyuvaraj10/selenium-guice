a61023609ce82ceb356df13b2bbfe4741c8506f2
Yuvaraj Gunisetti <Yuvaraj@Mac87-a334-d1cf.lan>
2017-07-27 23:58:23 +0100

    updated the webservice client

--- src/main/java/com/app/utils/WebServiceClient.java
+++ src/main/java/com/app/utils/WebServiceClient.java
@@ -2,10 +2,14 @@ package com.app.utils;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.http.*;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.*;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 
 import java.io.IOException;
@@ -27,18 +31,28 @@ public class WebServiceClient {
     private static WebServiceClient webServiceClient;
     private static HttpHost httpHost;
     private Map<String, String> headers;
+    private RequestConfig requestConfig;
 
     private WebServiceClient() {
         headers = new HashMap<>();
-        httpClient = HttpClientBuilder.create().build();
     }
 
     public static WebServiceClient build() {
+        httpClient = HttpClientBuilder.create().build();
         return getWebServiceClient();
     }
 
     public static WebServiceClient build(String hostName, int port, String schema) {
+        httpClient = HttpClientBuilder.create().build();
         httpHost = new HttpHost(hostName, port, schema);
+        return getWebServiceClient();
+    }
+
+    public WebServiceClient setSocketAndConnectTimeOut(int socketTimeOut, int connectTimeOut) {
+        requestConfig = RequestConfig.custom()
+                .setSocketTimeout(socketTimeOut)
+                .setConnectTimeout(connectTimeOut)
+                .build();
         return getWebServiceClient();
     }
 
@@ -61,41 +75,50 @@ public class WebServiceClient {
         switch (method.toUpperCase()) {
             case "GET": {
                 httpRequest = new HttpGet(url);
+                ((HttpGet)httpRequest).setConfig(requestConfig);
                 break;
             }
             case "POST": {
                 httpRequest = new HttpPost(url);
                 ((HttpPost)httpRequest).setEntity(httpEntity);
+                ((HttpPost)httpRequest).setConfig(requestConfig);
                 break;
             }
             case "PATCH": {
                 httpRequest = new HttpPatch(url);
                 ((HttpPatch)httpRequest).setEntity(httpEntity);
+                ((HttpPatch)httpRequest).setConfig(requestConfig);
                 break;
             }
             case "PUT": {
                 httpRequest = new HttpPut(url);
                 ((HttpPut)httpRequest).setEntity(httpEntity);
+                ((HttpPut)httpRequest).setConfig(requestConfig);
                 break;
             }
             case "OPTIONS": {
                 httpRequest = new HttpOptions(url);
+                ((HttpOptions)httpRequest).setConfig(requestConfig);
                 break;
             }
             case "HEAD": {
                 httpRequest = new HttpHead(url);
+                ((HttpHead)httpRequest).setConfig(requestConfig);
                 break;
             }
             case "TRACE": {
                 httpRequest = new HttpTrace(url);
+                ((HttpTrace)httpRequest).setConfig(requestConfig);
                 break;
             }
             case "DELETE": {
                 httpRequest = new HttpDelete(url);
+                ((HttpDelete)httpRequest).setConfig(requestConfig);
                 break;
             }
             default: {
                 httpRequest = new HttpGet(url);
+                ((HttpGet)httpRequest).setConfig(requestConfig);
                 break;
             }
         }
@@ -103,10 +126,19 @@ public class WebServiceClient {
     }
 
     public WebServiceClient sendAndReceive() throws Exception{
+        // response handler is added here so that user is relieved form worrying about connection management
+        // When using a ResponseHandler, HttpClient will automatically take care of ensuring release of
+        // the connection back to connection manager regardless whether the execution is succeeds or causes exception
+        ResponseHandler<HttpResponse> responseHandler = new ResponseHandler<HttpResponse>() {
+            @Override
+            public HttpResponse handleResponse(HttpResponse httpResponse) throws ClientProtocolException, IOException {
+               return httpResponse;
+            }
+        };
         if(httpHost == null) {
-            httpResponse = httpClient.execute((HttpUriRequest) httpRequest);
+            httpResponse = httpClient.execute((HttpUriRequest) httpRequest, responseHandler);
         } else {
-            httpResponse  = httpClient.execute(httpHost, httpRequest);
+            httpResponse  = httpClient.execute(httpHost, httpRequest, responseHandler);
         }
         return getWebServiceClient();
     }

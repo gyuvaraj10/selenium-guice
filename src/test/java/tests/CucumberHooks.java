b74dbe34a49f9426544500b0e7dba6950170c954
Yuvaraj Gunisetti <Yuvaraj@Mac87-a334-d1cf.uk.ad.ba.com>
2017-08-02 12:21:46 +0100

    removed the galen dependency

--- src/test/java/tests/CucumberHooks.java
+++ src/test/java/tests/CucumberHooks.java
@@ -1,11 +1,5 @@
 package tests;
 
-import com.galenframework.reports.GalenTestInfo;
-import com.galenframework.reports.HtmlReportBuilder;
-import com.galenframework.reports.TestReport;
-import com.galenframework.reports.model.LayoutReport;
-import com.galenframework.reports.nodes.LayoutReportNode;
-import com.galenframework.support.GalenReportsContainer;
 import com.google.inject.Inject;
 import cucumber.api.Scenario;
 import cucumber.api.java.After;
@@ -24,27 +18,15 @@ public class CucumberHooks {
     @Inject
     private WebDriver driver;
 
-    TestReport testReport;
-    GalenTestInfo ti;
-
     @Before
     public void beforeHook(Scenario scenario) {
-//        ti = new GalenTestInfo(scenario.getName(), null);
-//        testReport = new TestReport();
-//        ti.setReport(testReport);
-//        GalenReportsContainer.get().registerTest(scenario.getName(), null);
+
     }
     @After(order = 1)
     public void tearDown(Scenario scenario) throws Exception{
+        captureScreenShotOnFailure(scenario);
         driver.close();
         driver.quit();
-//        LayoutReport report1 = Report.getReport().getLayoutReport();
-//        TestReport report = GalenReportsContainer.get().getAllTests().get(0).getReport();
-//        report.addNode(new LayoutReportNode(report.getFileStorage(), report1, scenario.getName()));
-//        ti.setReport(report);
-//        HtmlReportBuilder builder = new HtmlReportBuilder();
-//        builder.build(Arrays.asList(ti),"target/");
-//        captureScreenShotOnFailure(scenario);
     }
 
     /**
